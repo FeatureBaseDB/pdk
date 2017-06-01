bdd410cbb912afd743fffe4671b280c1c8d00e43
Matt Jaffee <jaffee@umbel.com>
2017-06-01 13:23:30 -0500

    update readme and urls files, make default params more sensible

--- cmd/taxi.go
+++ cmd/taxi.go
@@ -28,8 +28,8 @@ func NewTaxiCommand(stdin io.Reader, stdout, stderr io.Writer) *cobra.Command {
 		},
 	}
 	flags := taxiCommand.Flags()
-	flags.IntVarP(&TaxiMain.Concurrency, "concurrency", "c", 1, "Number of goroutines fetching and parsing")
-	flags.IntVarP(&TaxiMain.BufferSize, "buffer-size", "b", 10000000, "Size of buffer for importers - heavily affects memory usage")
+	flags.IntVarP(&TaxiMain.Concurrency, "concurrency", "c", 8, "Number of goroutines fetching and parsing")
+	flags.IntVarP(&TaxiMain.BufferSize, "buffer-size", "b", 1000000, "Size of buffer for importers - heavily affects memory usage")
 	flags.StringVarP(&TaxiMain.PilosaHost, "pilosa", "p", "localhost:10101", "Pilosa host")
 	flags.StringVarP(&TaxiMain.Index, "index", "i", "taxi", "Pilosa db to write to")
 	flags.StringVarP(&TaxiMain.URLFile, "url-file", "f", "usecase/taxi/urls-short.txt", "File to get raw data urls from. Urls may be http or local files.")
