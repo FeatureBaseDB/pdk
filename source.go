291aa334f6168c52c9ed3dbda5854a3bad15bcee
Matt Jaffee <jaffee@pilosa.com>
2017-10-06 17:08:01 -0500

    document new ingester interfaces

--- source.go
+++ source.go
@@ -8,26 +8,39 @@ type Source interface {
 	Record() ([]byte, error)
 }
 
+// Parser is the interface for turning raw records from Source into Go objects.
+// Implementations of Parser should be thread safe. The current naming is a
+// temporary workaround until the previous Parser interface can be deprecated.
 type Parrrser interface {
 	Parse(record []byte) (interface{}, error)
 }
 
+// Mapper is the interface for taking parsed records from the Parser and
+// figuring out what bits and values to set in Pilosa. Mappers usually have a
+// Translator and a Nexter for converting arbitrary values to monotonic integer
+// ids and generating column ids respectively.
 type Mapppper interface {
 	Map(parsedRecord interface{}) (PilosaRecord, error)
 }
 
+// PilosaRecord represents a number of set bits and values in a single Column
+// in Pilosa.
 type PilosaRecord struct {
 	Col  uint64
 	Bits []SetBit
 	Vals []Val
 }
 
+// SetBit represents a bit to set in Pilosa sans column id (which is held by the
+// PilosaRecord containg the SetBit).
 type SetBit struct {
 	Frame string
 	Row   uint64
 	Time  time.Time
 }
 
+// Val represents a BSI value to set in a Pilosa field sans column id (which is
+// held by the PilosaRecord containg the Val).
 type Val struct {
 	Frame string
 	Field string
