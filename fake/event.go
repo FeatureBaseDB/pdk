5eccb697f9a4957aa7165ba3d9d127e5681d72df
Matt Jaffee <jaffee@pilosa.com>
2018-03-13 14:47:11 -0500

    add fake data generation utilities and fake Event

---
+++ fake/event.go
@@ -0,0 +1,141 @@
+package fake
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/pilosa/pdk/fake/gen"
+)
+
+// Event is an example event that includes a variety of types.
+type Event struct {
+	// ID is a unique event identifier
+	ID string `json:"id"`
+
+	// Station is a medium cardinality (1000s) string that has a 1 to many association with Events
+	Station string `json:"name"`
+
+	// UserID is a high cardinality (100s of millions) identifier that has a 1 to many association with events.
+	UserID int `json:"user_id"`
+
+	// Time the event occured. TODO how is it encoded?
+	Timestamp string `json:"timestamp"`
+
+	// A set of medium cardinality items to associate with this event.
+	Favorites []string `json:"favorites"`
+
+	// Set of complex objects.
+	Items []Item
+
+	// Ordered list of objects.
+	Ranking []Item
+
+	// An integer with a set range of possible values to associate with this event.
+	Velocity int `json:"velocity"`
+
+	// A boolean to associate with this event.
+	Active bool
+
+	// The location of this event.
+	Geo Geo `json:"geo"`
+}
+
+// Geo represents a location.
+type Geo struct {
+	// Low cardinality association.
+	TimeZone string `json:"time_zone"`
+
+	// Fine grained location.
+	Longitude float64 `json:"longitude"`
+	Latitude  float64 `json:"latitude"`
+}
+
+// Item has a name and integer value.
+type Item struct {
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+// GenEvent builds a random Event.
+func GenEvent() *Event {
+	active := false
+	if rand.Intn(2) > 0 {
+		active = true
+	}
+	return &Event{
+		ID:        fmt.Sprintf("%d", rand.Uint64()),
+		Station:   gen.String(6, 2000),
+		UserID:    int(gen.Uint64(100000000)) + 1,
+		Timestamp: gen.Time(time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC), time.Second*3).Format(time.RFC3339),
+		Favorites: genFavorites(),
+		Items:     genItems(),
+		Ranking:   genItems(),
+		Velocity:  rand.Intn(1000) + 2500,
+		Active:    active,
+		Geo:       genGeo(),
+	}
+}
+
+func genFavorites() []string {
+	n := rand.Intn(7)
+	favs := make([]string, n)
+	for i := 0; i < n; i++ {
+		done := false
+		for !done {
+			done = true
+			favs[i] = gen.String(8, 1000)
+			for j := 0; j < i; j++ {
+				if favs[i] == favs[j] {
+					done = false
+					break
+				}
+			}
+		}
+	}
+	return favs
+}
+
+var timezones = []string{
+	"ACDT", "ACST", "ACT", "ACT", "ACWST", "ADT", "AEDT", "AEST", "AFT", "AKDT", "AKST", "AMST",
+	"AMT", "AMT", "ART", "AST", "AST", "AWST", "AZOST", "AZOT", "AZT", "BDT", "BIOT", "BIT",
+	"BOT", "BRST", "BRT", "BST", "BST", "BST", "BTT", "CAT", "CCT", "CDT", "CDT", "CEST",
+	"CET", "CHADT", "CHAST", "CHOT", "CHOST", "CHST", "CHUT", "CIST", "CIT", "CKT", "CLST", "CLT",
+	"COST", "COT", "CST", "CST", "CST", "CT", "CVT", "CWST", "CXT", "DAVT", "DDUT", "DFT",
+	"EASST", "EAST", "EAT", "ECT", "ECT", "EDT", "EEST", "EET", "EGST", "EGT", "EIT", "EST",
+	"FET", "FJT", "FKST", "FKT", "FNT", "GALT", "GAMT", "GET", "GFT", "GILT", "GIT", "GMT",
+	"GST", "GST", "GYT", "HDT", "HAEC", "HST", "HKT", "HMT", "HOVST", "HOVT", "ICT", "IDT",
+	"IOT", "IRDT", "IRKT", "IRST", "IST", "IST", "IST", "JST", "KGT", "KOST", "KRAT", "KST",
+	"LHST", "LHST", "LINT", "MAGT", "MART", "MAWT", "MDT", "MET", "MEST", "MHT", "MIST", "MIT",
+	"MMT", "MSK", "MST", "MST", "MUT", "MVT", "MYT", "NCT", "NDT", "NFT", "NPT", "NST",
+	"NT", "NUT", "NZDT", "NZST", "OMST", "ORAT", "PDT", "PET", "PETT", "PGT", "PHOT", "PHT",
+	"PKT", "PMDT", "PMST", "PONT", "PST", "PST", "PYST", "PYT", "RET", "ROTT", "SAKT", "SAMT",
+	"SAST", "SBT", "SCT", "SDT", "SGT", "SLST", "SRET", "SRT", "SST", "SST", "SYOT", "TAHT",
+	"THA", "TFT", "TJT", "TKT", "TLT", "TMT", "TRT", "TOT", "TVT", "ULAST", "ULAT", "USZ1",
+	"UTC", "UYST", "UYT", "UZT", "VET", "VLAT", "VOLT", "VOST", "VUT", "WAKT", "WAST", "WAT",
+	"WEST", "WET", "WIT", "WST", "YAKT", "YEKT",
+}
+
+func genGeo() Geo {
+	return Geo{
+		TimeZone:  timezones[gen.Uint64(len(timezones))],
+		Longitude: float64(gen.Uint64(360000)) / 1000.0,
+		Latitude:  (float64(gen.Uint64(180000)) / 1000.0) - 90.0,
+	}
+}
+
+func genItems() []Item {
+	n := rand.Intn(5)
+	items := make([]Item, n)
+	for i := 0; i < n; i++ {
+		items[i] = genItem()
+	}
+	return items
+}
+
+func genItem() Item {
+	return Item{
+		Name:  gen.String(8, 1000),
+		Value: int(rand.Int31()),
+	}
+}
---
+++ fake/gen/generator.go
@@ -0,0 +1,160 @@
+package gen
+
+import (
+	"crypto/sha1"
+	"encoding/base32"
+	"encoding/binary"
+	"hash"
+	"math/rand"
+	"time"
+)
+
+// Generator holds state for generating random data in certain distributions.
+type Generator struct {
+	r     *rand.Rand
+	zs    map[int]*rand.Zipf
+	times map[time.Time]time.Duration
+	hsh   hash.Hash
+}
+
+// NewGenerator gets a new Generator
+func NewGenerator(seed int64) *Generator {
+	r := rand.New(rand.NewSource(seed))
+	return &Generator{
+		r:     r,
+		zs:    make(map[int]*rand.Zipf),
+		times: make(map[time.Time]time.Duration),
+		hsh:   sha1.New(),
+	}
+}
+
+var globalGen = NewGenerator(0)
+
+// String gets a zipfian random string from a set with the given cardinality.
+func (g *Generator) String(length, cardinality int) string {
+	if length > 32 {
+		length = 32
+	}
+
+	val := g.Uint64(cardinality)
+
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, val)
+	g.hsh.Write(b) // no need to check err
+	hashed := g.hsh.Sum(nil)
+	g.hsh.Reset()
+	return base32.StdEncoding.EncodeToString(hashed)[:length]
+}
+
+// Uint64 gets a zipfian random uint64 with the given cardinality.
+func (g *Generator) Uint64(cardinality int) uint64 {
+	z, ok := g.zs[cardinality]
+	if !ok {
+		imax := uint64(cardinality)
+		v := 0.05 * float64(imax)
+		if v < 1.0 {
+			v = 1.0
+		}
+		z = rand.NewZipf(g.r, 1.1, v, imax)
+		g.zs[cardinality] = z
+	}
+	return z.Uint64()
+}
+
+// Time returns a time increasing from the "from" time with a random delta.
+func (g *Generator) Time(from time.Time, maxDelta time.Duration) time.Time {
+	delta, ok := g.times[from]
+	if !ok {
+		delta = time.Duration(g.r.Uint64() % uint64(maxDelta))
+		g.times[from] = delta
+	} else {
+		delta += time.Duration(g.r.Uint64() % uint64(maxDelta))
+	}
+	return from.Add(delta)
+}
+
+// Global convenience funcs
+
+// String returns a random string with the given length (<=32), from a set of
+// possible strings of size "cardinality".
+func String(length, cardinality int) string {
+	return globalGen.String(length, cardinality)
+}
+
+// Uint64 gets a zipfian random uint64 with the given cardinality.
+func Uint64(cardinality int) uint64 {
+	return globalGen.Uint64(cardinality)
+}
+
+// Time returns a time increasing from the "from" time with a random delta.
+func Time(from time.Time, maxDelta time.Duration) time.Time {
+	return globalGen.Time(from, maxDelta)
+}
+
+// Permutation stuff
+
+// PermutationGenerator provides a way to pass integer IDs through a permutation
+// map that is pseudorandom but repeatable. This could be done with rand.Perm,
+// but that would require storing a [Iterations]int64 array, which we want to avoid
+// for large values of Iterations.
+// It works by using a Linear Congruence Generator (https://en.wikipedia.org/wiki/Linear_congruential_generator)
+// with modulus m = Iterations,
+// c = an arbitrary prime,
+// a = computed to ensure the full period.
+// relevant stackoverflow: http://cs.stackexchange.com/questions/29822/lazily-computing-a-random-permutation-of-the-positive-integers
+type PermutationGenerator struct {
+	a int64
+	c int64
+	m int64
+}
+
+// NewPermutationGenerator returns a PermutationGenerator which will permute
+// numbers in 0-n.
+func NewPermutationGenerator(m int64, seed int64) *PermutationGenerator {
+	// figure out 'a' and 'c', return PermutationGenerator
+	a := multiplierFromModulus(m, seed)
+	c := int64(22695479)
+	return &PermutationGenerator{a, c, m}
+}
+
+// Permute gets the permuted value for n.
+func (p *PermutationGenerator) Permute(n int64) int64 {
+	// run one step of the LCG
+	return (n*p.a + p.c) % p.m
+}
+
+// LCG parameters must satisfy three conditions:
+// 1. m and c are relatively prime (satisfied for prime c != m)
+// 2. a-1 is divisible by all prime factors of m
+// 3. a-1 is divisible by 4 if m is divisible by 4
+// Additionally, a seed can be used to select between different permutations
+func multiplierFromModulus(m int64, seed int64) int64 {
+	factors := primeFactors(m)
+	product := int64(1)
+	for p := range factors {
+		// satisfy condition 2
+		product *= p
+	}
+
+	if m%4 == 0 {
+		// satisfy condition 3
+		product *= 2
+	}
+
+	return product*seed + 1
+}
+
+// Returns map of {integerFactor: count, ...}
+// This is a naive algorithm that will not work well for large prime n.
+func primeFactors(n int64) map[int64]int {
+	factors := make(map[int64]int)
+	for i := int64(2); i <= n; i++ {
+		div, mod := n/i, n%i
+		for mod == 0 {
+			factors[i]++
+			n = div
+			div, mod = n/i, n%i
+		}
+	}
+	return factors
+}
