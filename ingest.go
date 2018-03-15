6c81fe4bd891409406f47d819fc70558eb70ff4d
Matt Jaffee <jaffee@pilosa.com>
2018-03-15 14:46:00 -0500

    add debuging prints to ingest.go

--- ingest.go
+++ ingest.go
@@ -1,6 +1,7 @@
 package pdk
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"sync"
@@ -45,16 +46,19 @@ func (n *Ingester) Run() error {
 				if recordErr != nil {
 					break
 				}
+				fmt.Printf("got record %#v\n", rec)
 				val, err := n.parser.Parse(rec)
 				if err != nil {
 					log.Printf("couldn't parse record %s, err: %v", rec, err)
 					continue
 				}
+				fmt.Printf("parsed record %#v\n", val)
 				pr, err := n.mapper.Map(val)
 				if err != nil {
 					log.Printf("couldn't map val: %s, err: %v", val, err)
 					continue
 				}
+				fmt.Printf("mapped record %#v\n", pr)
 				for _, row := range pr.Rows {
 					n.indexer.AddBit(row.Frame, pr.Col, row.ID)
 				}
