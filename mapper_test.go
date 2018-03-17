ebd8516081002172dde16062e09da0f1d209353e
Matt Jaffee <jaffee@pilosa.com>
2018-03-17 18:12:36 -0500

    mapper fix for top level fields

--- mapper_test.go
+++ mapper_test.go
@@ -1,1 +1,30 @@
 package pdk_test
+
+import (
+	"testing"
+
+	"github.com/pilosa/pdk"
+)
+
+func TestCollapsingMapper(t *testing.T) {
+	cm := pdk.NewCollapsingMapper()
+	e := &pdk.Entity{
+		Subject: "blah",
+		Objects: map[pdk.Property]pdk.Object{
+			"aa": pdk.S("hello"),
+			"bb": pdk.I(49),
+		},
+	}
+	pr, err := cm.Map(e)
+	if err != nil {
+		t.Fatalf("mapping entity: %v", err)
+	}
+
+	if len(pr.Rows) != 1 {
+		t.Fatalf("wrong rows: %v", pr.Rows)
+	}
+	if len(pr.Vals) != 1 {
+		t.Fatalf("wrong vals: %v", pr.Vals)
+	}
+
+}
