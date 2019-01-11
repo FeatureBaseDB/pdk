63635e5a66fbe625bb5dafeb4b99017343949cd1
Matt Jaffee <matthew.jaffee@gmail.com>
2019-01-11 16:05:00 -0600

    disable existence tracking on taxi index

--- usecase/taxi/main.go
+++ usecase/taxi/main.go
@@ -132,7 +132,7 @@ func (m *Main) Run() error {
 	}
 
 	schema := gopilosa.NewSchema()
-	index := schema.Index(m.Index)
+	index := schema.Index(m.Index, gopilosa.OptIndexTrackExistence(false))
 
 	pdk.NewRankedField(index, "cab_type", 10)
 	pdk.NewRankedField(index, "passenger_count", 1000)
